1d6700ea528feedd5ba3e85a12a57b53b925f311
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-13 19:10:55 +0200

    new tenant model

---
+++ models/Tenant.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var TenantSchema = new mongoose.Schema({
+	email: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
+	accountHolder: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"] },
+	firstName: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'] },
+	lastName: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'] },
+	iban: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"] },
+	rent: Number,
+	phone: { type: Number },
+	uuId: { type: String, lowercase: true, unique: true, required: [true, "can't be blank"] },
+	userId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+}, { timestamps: true });
+
+TenantSchema.plugin(uniqueValidator, { message: 'is already taken.' });
+
+
+TenantSchema.methods.tenantToJSON = function () {
+	return {
+		id: this.id,
+		email: this.email,
+		accountHolder: this.accountHolder,
+		firstName: this.firstName,
+		lastName: this.lastName,
+		iban: this.iban,
+		rent: this.rent,
+		phone: this.phone,
+		userId: this.userId,
+		uuId: this.userRefId
+	};
+};
+
+
+mongoose.model('Tenant', TenantSchema);
