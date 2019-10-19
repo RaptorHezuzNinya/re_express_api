233bd509b3c4b118f07832c2eb7b1ce470b13996
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-19 15:23:29 +0200

    payment model

---
+++ models/Payment.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+// var uniqueValidator = require('mongoose-unique-validator');
+
+var PaymentSchema = new mongoose.Schema({
+	name: { type: String, required: [true, "can't be blank"] },
+	iban: { type: String, required: [true, "can't be blank"] },
+	date: { type: String, required: [true, "can't be blank"] },
+	credited: { type: Boolean, required: [true, "can't be blank"] },
+	amount: { type: Number, required: [true, "can't be blank"] },
+	memo: { type: String },
+	userRef: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+	tenantRef: { type: mongoose.Schema.Types.ObjectId, ref: 'Tenant' }
+}, { timestamps: true });
+
+// PaymentSchema.plugin(uniqueValidator, { message: 'is already taken.' });
+
+PaymentSchema.methods.paymentToJSON = function () {
+	return {
+		_id: this.id,
+		name: this.name,
+		iban: this.iban,
+		date: this.date,
+		credited: this.credited,
+		amount: this.amount,
+		meno: this.meno,
+
+		userRef: this.userRef,
+		tenantRef: this.tenantRef,
+
+		updatedAt: this.updatedAt,
+		createdAt: this.createdAt
+	};
+};
+
+
+mongoose.model('Payment', PaymentSchema);
