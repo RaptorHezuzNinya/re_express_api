b89a5f638f0cf65acb526658de0b8adf3d07ddd4
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-19 15:23:02 +0200

    format

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var http = require('http'),
-  path = require('path'),
-  methods = require('methods'),
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  session = require('express-session'),
-  cors = require('cors'),
-  passport = require('passport'),
-  errorhandler = require('errorhandler'),
-  mongoose = require('mongoose');
+	path = require('path'),
+	methods = require('methods'),
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	session = require('express-session'),
+	cors = require('cors'),
+	passport = require('passport'),
+	errorhandler = require('errorhandler'),
+	mongoose = require('mongoose');
 
 var isProduction = process.env.NODE_ENV === 'production';
 
@@ -27,20 +27,21 @@ app.use(express.static(__dirname + '/public'));
 app.use(session({ secret: 'conduit', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
 
 if (!isProduction) {
-  app.use(errorhandler());
+	app.use(errorhandler());
 }
 
 if (isProduction) {
-  mongoose.connect(process.env.MONGODB_URI);
+	mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect('mongodb://localhost/conduit');
-  mongoose.set('debug', true);
+	mongoose.connect('mongodb://localhost/conduit');
+	mongoose.set('debug', true);
 }
 
 require('./models/User');
 require('./models/Article');
 require('./models/Comment');
 require('./models/Tenant');
+require('./models/Payment');
 require('./config/passport');
 
 app.use(require('./routes'));
@@ -48,9 +49,9 @@ app.use(require('./routes'));
 
 /// catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 /// error handlers
@@ -58,33 +59,33 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (!isProduction) {
-  app.use(function (err, req, res, next) {
-    console.log(err.stack);
+	app.use(function (err, req, res, next) {
+		console.log(err.stack);
 
-    res.status(err.status || 500);
+		res.status(err.status || 500);
 
-    res.json({
-      'errors': {
-        message: err.message,
-        error: err
-      }
-    });
-  });
+		res.json({
+			'errors': {
+				message: err.message,
+				error: err
+			}
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    'errors': {
-      message: err.message,
-      error: {}
-    }
-  });
+	res.status(err.status || 500);
+	res.json({
+		'errors': {
+			message: err.message,
+			error: {}
+		}
+	});
 });
 
 // finally, let's start our server...
 var server = app.listen(process.env.PORT || 3000, function () {
-  console.log('Listening on port ' + server.address().port);
+	console.log('Listening on port ' + server.address().port);
 });
