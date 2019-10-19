f4c83bf15ae329aa0ad0556ffc2c832b5d05e065
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-19 15:23:18 +0200

    removed

--- routes/api/users.js
+++ routes/api/users.js
@@ -13,17 +13,6 @@ router.get('/user', auth.required, function (req, res, next) {
 	}).catch(next);
 });
 
-// 				WIP 
-// router.get('/user/tenants', auth.required, function (req, res, next) {
-// 	console.log(req.payload)
-// 	User.findById(req.payload.id).then(function (user) {
-// 		if (!user) { return res.sendStatus(401); }
-
-// 		return res.json({ user: user.toAuthJSON() });
-// 	}).catch(next);
-// });
-
-
 router.put('/user', auth.required, function (req, res, next) {
 	User.findById(req.payload.id).then(function (user) {
 		if (!user) { return res.sendStatus(401); }
