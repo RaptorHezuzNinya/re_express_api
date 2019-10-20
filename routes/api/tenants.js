6ea5174dc6508e670f2b3283b26c8bf61098cc6e
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-20 02:17:48 +0200

    update routes

--- routes/api/tenants.js
+++ routes/api/tenants.js
@@ -37,5 +37,35 @@ router.post('/tenant', auth.required, function (req, res, next) {
 
 });
 
+router.post('/tenants', auth.required, (req, res, next) => {
+    User.findById(req.payload.id).then((user) => {
+        if (!user) { return res.sendStatus(401); }
+        const tenant = new Tenant();
+        console.log(tenant);
+
+        for (let index = 0; index < req.body.length; index++) {
+            const newTenant = req.body[index];
+
+        }
+
+        // tenant.email = req.body.tenant.email;
+        // tenant.accountHolder = req.body.tenant.accountHolder;
+        // tenant.firstName = req.body.tenant.firstName;
+        // tenant.lastName = req.body.tenant.lastName;
+        // tenant.iban = req.body.tenant.iban;
+        // tenant.rent = req.body.tenant.rent;
+        // tenant.phone = req.body.tenant.phone;
+        // tenant.userRef = user._id;
+        // tenant.uuId = uid.randomUUID(9);
+        // tenant.save().then(function () {
+        //     user.setTenantRef(tenant);
+        //     user.save().then(function () {
+        //         return res.json({ tenant: tenant.tenantToJSON() });
+        //     })
+        // }).catch(next);
+
+
+    });
+});
 
 module.exports = router;
