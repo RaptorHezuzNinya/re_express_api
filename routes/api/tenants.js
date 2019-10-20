98ec5c4ff25923133ef355554e400e31b7931ef8
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-20 18:14:51 +0200

    refactor into own methods

--- routes/api/tenants.js
+++ routes/api/tenants.js
@@ -1,15 +1,16 @@
-var mongoose = require('mongoose');
-var router = require('express').Router();
-var Tenant = mongoose.model('Tenant');
-var User = mongoose.model('User');
-var auth = require('../auth');
+const asyncMiddleware = require('../middleware/async.mw.js');
+const mongoose = require('mongoose');
+const router = require('express').Router();
+const Tenant = mongoose.model('Tenant');
+const User = mongoose.model('User');
+const auth = require('../auth');
 
-var ShortUniqueId = require('short-unique-id');
-var uid = new ShortUniqueId();
+const ShortUniqueId = require('short-unique-id');
+const uid = new ShortUniqueId();
 
 // get tenants by user ref
-router.get('/tenants/:userRef', function(req, res, next) {
-	Tenant.find({ userRef: req.params.userRef })
+router.get('/tenants', auth.required, function(req, res, next) {
+	Tenant.find({ userRef: req.payload.id })
 		.then(function(tenants) {
 			return res.json({ tenants: tenants });
 		})
@@ -18,7 +19,7 @@ router.get('/tenants/:userRef', function(req, res, next) {
 
 router.post('/tenant', auth.required, function(req, res, next) {
 	User.findById(req.payload.id).then(function(user) {
-		var tenant = new Tenant();
+		const tenant = new Tenant();
 
 		tenant.email = req.body.tenant.email;
 		tenant.accountHolder = req.body.tenant.accountHolder;
@@ -32,7 +33,7 @@ router.post('/tenant', auth.required, function(req, res, next) {
 		tenant
 			.save()
 			.then(function() {
-				user.setTenantRef(tenant);
+				// user.setTenantRef(tenant);
 				user.save().then(function() {
 					return res.json({ tenant: tenant.tenantToJSON() });
 				});
@@ -41,32 +42,41 @@ router.post('/tenant', auth.required, function(req, res, next) {
 	});
 });
 
-router.post('/tenants', auth.required, (req, res, next) => {
-	Promise.all([User.findById(req.payload.id)])
-		.then(results => {
-			const user = results[0];
-			const createdDocuments = [];
-			for (let index = 0; index < req.body.length; index++) {
-				const newTenant = req.body[index];
+router.post('/tenants', auth.required, async (req, res, next) => {
+	try {
+		const user = await getUser(req.payload.id);
+		const tenants = await insertTenants(req.body, user);
 
-				const tenant = new Tenant();
-
-				tenant.email = newTenant.email;
-				tenant.accountHolder = newTenant.accountHolder;
-				tenant.firstName = newTenant.firstName;
-				tenant.lastName = newTenant.lastName;
-				tenant.iban = newTenant.iban;
-				tenant.rent = newTenant.rent;
-				tenant.phone = newTenant.phone;
-				tenant.userRef = user._id;
-				tenant.uuId = uid.randomUUID(9);
-				tenant.save().then(() => {
-					createdDocuments.push({ ...tenant.tenantToJSON() });
-				});
-			}
-			return res.sendStatus(200).res.json({ tenants: createdDocuments });
-		})
-		.catch(next);
+		res.json(tenants);
+	} catch (error) {
+		console.error('ERROR! -> ', error);
+		req.json(error);
+	}
 });
+
+const getUser = id => {
+	return User.findById(id).then(user => {
+		return user;
+	});
+};
+
+const insertTenants = (tenants, user) => {
+	const insertedDocs = tenants.map(newTenant => {
+		const tenant = new Tenant();
+
+		tenant.email = newTenant.email;
+		tenant.accountHolder = newTenant.accountHolder;
+		tenant.firstName = newTenant.firstName;
+		tenant.lastName = newTenant.lastName;
+		tenant.iban = newTenant.iban;
+		tenant.rent = newTenant.rent;
+		tenant.phone = newTenant.phone;
+		tenant.userRef = user._id;
+		tenant.uuId = uid.randomUUID(9);
+		tenant.save();
+		return { ...tenant.tenantToJSON() };
+	});
+	return insertedDocs;
+};
 
 module.exports = router;
