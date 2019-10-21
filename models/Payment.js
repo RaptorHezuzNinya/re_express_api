d8b205f33049e8d82c06d70fb6e37247204d7d13
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-21 21:05:42 +0200

    str for now

--- models/Payment.js
+++ models/Payment.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-
+var uniqueValidator = require('mongoose-unique-validator');
 const PaymentSchema = new mongoose.Schema(
 	{
 		name: { type: String, required: [true, "can't be blank"] },
 		iban: { type: String, required: [true, "can't be blank"] },
 		date: { type: String, required: [true, "can't be blank"] },
 		credited: { type: Boolean, required: [true, "can't be blank"] },
-		amount: { type: Number, required: [true, "can't be blank"] },
+		amount: { type: String, required: [true, "can't be blank"] },
 		memo: { type: String },
 		userRef: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 		tenantRef: { type: mongoose.Schema.Types.ObjectId, ref: 'Tenant' }
