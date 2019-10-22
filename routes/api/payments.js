b6acee3acde4c27c9e5c05e0c2419d4c9bf1fac3
RaptorHezuzNinya <w22243vrh@gmail.com>
2019-10-22 02:01:15 +0200

    removed

--- routes/api/payments.js
+++ routes/api/payments.js
@@ -5,70 +5,33 @@ const User = mongoose.model('User');
 const Tenant = mongoose.model('Tenant');
 const Payment = mongoose.model('Payment');
 
-router.post('/payments/user', auth.required, async (req, res, next) => {
-	try {
-		const user = await getUser(req.payload.id);
-		await insertPayments(req.body, user);
-		// so far works
-		let holder;
-		Promise.resolve(user ? Payment.find({ userRef: user._id }) : null)
-			.then(payments => {
-				const payload = payments.map(payment => {
-					return { ...payment.paymentToJSON() };
+router.post('/payments', auth.required, (req, res, next) => {
+	User.findById(req.payload.id) // query return promise
+		.then(user => {
+			for (let index = 0; index < req.body.length; index++) {
+				const payment = req.body[index];
+				Tenant.findOne({ iban: payment.iban }).then(tenant => {
+					const newPayment = new Payment();
+					newPayment.name = payment.name;
+					newPayment.iban = payment.iban;
+					newPayment.date = payment.date;
+					newPayment.credited = payment.credited === 'Bij' ? true : false;
+					newPayment.amount = payment.amount;
+					newPayment.memo = payment.memo;
+					newPayment.userRef = user._id;
+					newPayment.tenantRef = tenant._id;
+					newPayment.save();
 				});
-				return res.json(payload);
-			})
-			.catch(next);
-	} catch (error) {
-		console.error('ERROR! ->', error);
-	}
+			}
+		})
+		.then(() => {
+			User.findById(req.payload.id).then(user => {
+				Payment.find({ userRef: user._id }).then(payments => {
+					return res.json(payments);
+				});
+			});
+		})
+		.catch(next);
 });
 
-const getUser = id => {
-	return User.findById(id).then(user => {
-		return user;
-	});
-};
-
-const insertPayments = (payments, user) => {
-	payments.forEach(payment => {
-		Tenant.findOne({ iban: payment.iban }).then(tenant => {
-			const newPayment = new Payment();
-
-			newPayment.name = payment.name;
-			newPayment.iban = payment.iban;
-			newPayment.date = payment.date;
-			newPayment.credited = payment.credited;
-			newPayment.amount = payment.amount;
-			newPayment.memo = payment.memo;
-			newPayment.userRef = user._id;
-			newPayment.tenantRef = tenant._id;
-			newPayment.save((err, newPayment) => {
-				// console.log(newPayment);
-				if (err) {
-					console.log(err);
-				}
-			});
-		});
-	});
-};
-
 module.exports = router;
-
-// const insertedDocs = payments.map(payment => {
-// 	Tenant.findOne({ iban: payment.iban }).then(tenant => {
-// 		const newPayment = new Payment();
-
-// 		newPayment.name = payment.name;
-// 		newPayment.iban = payment.iban;
-// 		newPayment.date = payment.date;
-// 		newPayment.credited = payment.credited;
-// 		newPayment.amount = payment.amount;
-// 		newPayment.memo = payment.memo;
-// 		newPayment.userRef = user._id;
-// 		newPayment.tenantRef = tenant._id;
-// 		newPayment.save();
-// 		return { ...newPayment.paymentToJSON() };
-// 	});
-// });
-// return insertedDocs;
